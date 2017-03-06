c9605d8645babaeefa091d74d0a10fa3c9c8ba94
Udey Rishi <udey.rishi@gmail.com>
2017-03-06 10:36:38 -0700

    Added component chip

---
+++ app/src/main/java/aaku492/smartfoodtracker/common/Chip.java
@@ -0,0 +1,86 @@
+package aaku492.smartfoodtracker.common;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import aaku492.smartfoodtracker.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Udey Rishi (udeyrishi) on 2017-03-06.
+ * Copyright © 2017 ECE 492 Group 2 (Winter 2017), University of Alberta. All rights reserved.
+ */
+public class Chip extends RelativeLayout {
+    @BindView(R.id.chip_text)
+    protected TextView chipText;
+    private boolean isSelected = false;
+    private OnSelectionChangedListener onSelectionChangedListener = null;
+
+    public Chip(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public Chip(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+        initAttrs(attrs);
+    }
+
+    public Chip(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context);
+        initAttrs(attrs);
+    }
+
+    private void init(final Context context) {
+        View.inflate(context, R.layout.component_chip, this);
+        this.setClickable(true);
+        ButterKnife.bind(this);
+        super.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isSelected = !isSelected;
+                setSelected(isSelected);
+                if (onSelectionChangedListener != null) {
+                    onSelectionChangedListener.onSelectionChanged(Chip.this, isSelected);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void setOnClickListener(@Nullable OnClickListener onClickListener) {
+        throw new UnsupportedOperationException("Please use setOnSelectionChangedListener instead.");
+    }
+
+    public void setOnSelectionChangedListener(@Nullable OnSelectionChangedListener onSelectionChangedListener) {
+        this.onSelectionChangedListener = onSelectionChangedListener;
+    }
+
+    private void initAttrs(AttributeSet attrs) {
+        int[] set = { android.R.attr.text };
+        TypedArray a = getContext().obtainStyledAttributes(attrs, set);
+        String text = a.getString(0);
+        setText(text);
+        a.recycle();
+    }
+
+    public void setText(CharSequence text) {
+        this.chipText.setText(text);
+    }
+
+    public CharSequence getText() {
+        return chipText.getText();
+    }
+
+    public interface OnSelectionChangedListener {
+        void onSelectionChanged(Chip v, boolean selected);
+    }
+}
