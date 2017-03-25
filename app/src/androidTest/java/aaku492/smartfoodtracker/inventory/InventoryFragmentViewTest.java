bf90ae1c5b9224daa267fdd1da2ca9fd3d165c6f
Udey Rishi <udey.rishi@gmail.com>
2017-03-25 17:41:32 -0600

    Added InventoryFragmentViewTest

---
+++ app/src/androidTest/java/aaku492/smartfoodtracker/inventory/InventoryFragmentViewTest.java
@@ -0,0 +1,31 @@
+package aaku492.smartfoodtracker.inventory;
+
+import com.facebook.testing.screenshot.Screenshot;
+import com.facebook.testing.screenshot.ViewHelpers;
+
+import org.junit.Test;
+
+import java.text.ParseException;
+
+import aaku492.smartfoodtracker.BaseScreenshotTest;
+
+/**
+ * Created by Udey Rishi (udeyrishi) on 2017-03-25.
+ * Copyright © 2017 ECE 492 Group 2 (Winter 2017), University of Alberta. All rights reserved.
+ */
+public class InventoryFragmentViewTest extends BaseScreenshotTest {
+    @Test
+    public void testRender() throws ParseException {
+        InventoryFragmentView view = InventoryFragmentView.inflate(getLayoutInflater(), null, null);
+        InventoryAdapter adapter = new InventoryAdapter(getTestInventoryItems(), null);
+        view.render(adapter);
+        adapter.notifyDataSetChanged();
+        ViewHelpers.setupView(view)
+                .setExactWidthDp(300)
+                .setExactHeightDp(400)
+                .layout();
+
+        Screenshot.snap(view)
+                .record();
+    }
+}
