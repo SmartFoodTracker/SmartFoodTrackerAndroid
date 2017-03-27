b9991f7562c7ecafec3a9fe9bff396c362165913
Udey Rishi <udey.rishi@gmail.com>
2017-03-27 16:00:40 -0600

    Added RecipeListFragmentViewTest.testRenderWithNoRecipes

--- app/src/androidTest/java/aaku492/smartfoodtracker/recipes/RecipeListFragmentViewTest.java
+++ app/src/androidTest/java/aaku492/smartfoodtracker/recipes/RecipeListFragmentViewTest.java
@@ -2,6 +2,8 @@ package aaku492.smartfoodtracker.recipes;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import aaku492.smartfoodtracker.BaseScreenshotTest;
 
 import static aaku492.smartfoodtracker.BaseScreenshotTest.ScreenshotTaker.DEFAULT_DELAY_MS;
@@ -12,11 +14,19 @@ import static aaku492.smartfoodtracker.TestFixtures.getRecipes;
  * Created by Andrew Hoskins (ahoskins) on 2017-03-26.
  * Copyright © 2017 ECE 492 Group 2 (Winter 2017), University of Alberta. All rights reserved.
  */
-public class RecipeListFragmentViewTest  extends BaseScreenshotTest {
+public class RecipeListFragmentViewTest extends BaseScreenshotTest {
     @Test
     public void testRender() {
+        runTest(new RecipesHomeAdapter(getRecipes(), null));
+    }
+
+    @Test
+    public void testRenderWithNoRecipes() {
+        runTest(new RecipesHomeAdapter(new ArrayList<RecipeResponse.Recipe>(), null));
+    }
+
+    private void runTest(final RecipesHomeAdapter adapter) {
         final RecipesListFragmentView[] view = new RecipesListFragmentView[1];
-        final RecipesHomeAdapter adapter = new RecipesHomeAdapter(getRecipes(), null);
 
         runOnMainSync(new Runnable() {
             @Override
